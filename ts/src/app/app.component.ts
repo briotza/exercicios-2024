9ead8ea25eb471171345bed4aac265f5a20f3cb0
briotza <cianomeliunas@gmail.com>
2024-05-01 11:52:53 -0300

    feat: comentei componentes

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -15,30 +15,36 @@ export class AppComponent {
   topicOpen = false;
   hiddenBar = false;
 
+  //expandir resumo
   readMore() {
     this.showMore = !this.showMore;
   }
 
+  //botão criar tópico e criar novo tópico
   createTopic() {
     this.showIdeas = false;
     this.createTopicClicked = true;
   }
 
+  //botão enviar
   send() {
     this.createTopicClicked = false;
     this.topicSent = true;
   }
 
+  //abrir comentários do tópico
   openTopic() {
     this.topicClosed = false;
     this.topicOpen = true;
   }
 
+  //fechar comentários do tópico
   closeTopic() {
     this.topicClosed = true;
     this.topicOpen = false;
   }
 
+  //abrir barra de navegação em telas menores
   openHiddenBar(){
     this.hiddenBar = !this.hiddenBar;
   }
